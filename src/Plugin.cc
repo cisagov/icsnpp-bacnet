a9f44475a9548dd723a6a1f1830c2e8233785a77
Kleinspider <stephen.kleinheider@gmail.com>
2023-06-12 10:37:45 -0600

    Version update: 1.2 -> 1.3

--- src/Plugin.cc
+++ src/Plugin.cc
@@ -21,7 +21,7 @@ zeek::plugin::Configuration Plugin::Configure()
     config.name = "ICSNPP::BACnet";
     config.description = "BACnet Protocol analyzer";
     config.version.major = 1;
-    config.version.minor = 2;
+    config.version.minor = 3;
 
     return config;
 }
